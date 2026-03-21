132f876f65a56857130854720b1adbe1aaf25a49
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-21 19:51:52 +0530

    Time: 2 ms (85.71%) | Memory: 47.5 MB (69.59%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -17,7 +17,9 @@ class Solution {
                 break;
             }
         }
-        if(idx<=0) return false;
+        if(idx<=0) return false;// peak cant be the nums [0] ans there should be
+        //both parts-> strict inc-peak- strict dec.. exp-> [3,2,1] ->peak=nums[0] but mountain  doesnt have a inc part
+
         //after peak all should decrease.
         for(int i=idx+1;i<n;i++)
         {
