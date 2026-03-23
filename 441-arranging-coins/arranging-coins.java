7242ed0358cbf1e34dc7a97cc1acf0fb8f204caa
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-23 23:17:50 +0530

    Time: 1 ms (99.96%) | Memory: 42.5 MB (87.53%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,27 +1,6 @@
 class Solution {
-    public boolean canArrange(long x,int n)
-    {
-        long value=x*(x+1)/2;
-        return value<=n;
-    }
     public int arrangeCoins(int n) 
     {
-        long low=1;
-        long high=n;
-        while(low<=high)
-        {
-            long mid=low+(high-low)/2;
-            if(canArrange(mid,n))
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-        }
-        return (int)high;
-
-        
+        return (int)((Math.sqrt(1 + 8.0 * n) - 1) / 2);
     }
 }
