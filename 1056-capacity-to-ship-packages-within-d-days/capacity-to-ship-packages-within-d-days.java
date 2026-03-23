c5d81d7a86e3711f5a1017c3903d69b5f19e9d31
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-23 17:53:00 +0530

    Time: 11 ms (70.17%) | Memory: 50.6 MB (14.18%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,51 @@
+class Solution {
+    private boolean isPossible(int[]nums,int days,long wt)
+    {
+        long d=0;
+        long sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(sum+nums[i]<=wt)
+            {
+                sum+=nums[i];
+            }
+            else
+            {
+               d++;
+               sum=nums[i];
+               if(d>days) return false;
+
+            }
+
+        }
+        if(sum<=wt) d++;
+        return d<=days;
+
+    }
+    public int shipWithinDays(int[] weights, int days) 
+    {
+        long low=1;
+        long high=0;
+        for(int x:weights)
+        {
+            low=Math.max(low, x);
+            high+=x;
+        }
+        while(low<=high)
+        {
+            long mid=low+(high-low)/2;
+            if(isPossible(weights,days,mid))
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+
+        return (int)low;
+        
+    }
+}
