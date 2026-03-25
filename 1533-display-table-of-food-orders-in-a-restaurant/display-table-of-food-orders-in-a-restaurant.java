a1bd02317a0560bb7bbdd136dd42a6518ce143df
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-25 21:45:50 +0530

    Time: 33 ms (71.75%) | Memory: 54.4 MB (80.79%) - LeetSync

--- 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
@@ -2,12 +2,14 @@ class Solution {
     public List<List<String>> displayTable(List<List<String>> orders) 
     {
         int n=orders.size();
-         Map<String, Map<String, Integer>> map = new TreeMap<>((a,b) -> Integer.parseInt(a)-Integer.parseInt(b));
-        Set<String> items=new HashSet<>();
+        //  Map<String, Map<String, Integer>> map = new TreeMap<>((a,b) -> Integer.parseInt(a)-Integer.parseInt(b));
+        Map<Integer, Map<String, Integer>> map=new TreeMap<>();
+        Set<String> items = new HashSet<>();
+       
         for(int i=0;i<n;i++)
         {
-            String table = orders.get(i).get(1);
-            String item = orders.get(i).get(2);
+            int table=Integer.parseInt(orders.get(i).get(1));
+            String item=orders.get(i).get(2);
             if(!items.contains(item))
             {
                 items.add(item);
@@ -26,12 +28,12 @@ class Solution {
         header.addAll(food);
         List<List<String>> content=new ArrayList<>();
         content.add(header);
-        for(Map.Entry<String,Map<String,Integer>> data:map.entrySet())
+        for(Map.Entry<Integer,Map<String,Integer>> data:map.entrySet())
         {
-            String table=data.getKey();
+            int table=data.getKey();
             Map<String,Integer> fI=data.getValue();
             List<String> enter=new ArrayList<>();
-            enter.add(table);
+            enter.add(table+"");
             for(int i=0;i<food.size();i++)
             {
                 String f=food.get(i);
