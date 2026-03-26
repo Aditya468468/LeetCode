1282df4301d7e251420c4a5e3c6716fe10150abf
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-26 18:17:53 +0530

    Time: 2 ms (47.67%) | Memory: 48.4 MB (97.45%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -6,47 +6,49 @@ class Solution {
         int m=arr2.length;
         int i=0;
         int j=0;
-        ArrayList<Integer> list=new ArrayList<>();
+        int size=n+m;
+        int idx1=size/2;
+        int idx2=idx1-1;
+        int val1=0,val2=0;
+        int cnt=0;
+    
         while(i<n && j<m)
         {
-            if(arr1[i]==arr2[j])
+            if(arr1[i]<=arr2[j])
             {
-                list.add(arr1[i]);
+                if(idx1==cnt) val1=arr1[i];
+                if(idx2==cnt) val2=arr1[i];
+                cnt++;
                 i++;
             }
             else if(arr1[i]>arr2[j])
             {
-                list.add(arr2[j]);
+                if(idx1==cnt) val1=arr2[j];
+                if(idx2==cnt) val2=arr2[j];
+                cnt++;
                 j++;
             }
-            else{
-                list.add(arr1[i]);
-                i++;
-            }
+
         }
         while(i<n)
         {
-            list.add(arr1[i]);
+            if(idx1==cnt) val1=arr1[i];
+            if(idx2==cnt) val2=arr1[i];
             i++;
+            cnt++;
         }
         while(j<m)
         {
-            list.add(arr2[j]);
+            if(idx1==cnt) val1=arr2[j];
+            if(idx2==cnt) val2=arr2[j];
             j++;
+            cnt++;
         }
-        int size=list.size();
-        if(size%2==0)
-        {
-            int idx=size/2;
-            double median=list.get(idx)+list.get(idx-1);
-            return median/2;
-        }
-        else 
-        {
-            int idx=size/2;
-            double median=list.get(idx);
-            return median;
-        }
+        if(size%2==0) return ((double)val1+val2)/2;
+        else return (double)val1;
+        
+       
      
     }
 }
+
