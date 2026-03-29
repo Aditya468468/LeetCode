d942399bff5ead5c42e715adb658e8e9dd6b035f
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-30 02:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (16.44%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,30 +3,30 @@ class Solution {
     {
         int n=mat.length;
         int m=mat[0].length;
-        for(int i=0;i<n;i++)
+        int size=n*m;
+        int low=0;
+        int high=size-1;
+        while(low<=high)
         {
-            if(mat[i][0]<=target && target<=mat[i][m-1])
+            int mid=low+(high-low)/2;
+            int r=mid/m;
+            int c=mid%m;
+            if(mat[r][c]==target)
             {
-                int low=0;
-                int high=m-1;
-                while(low<=high)
-                {
-                    int mid=low+(high-low)/2;
-                    if(mat[i][mid]==target)
-                    {
-                        return true;
-                    }
-                    else if(mat[i][mid]>target)
-                    {
-                        high=mid-1;
-                    }
-                    else{
-                        low=mid+1;
-                    }
-                }
+                return true;
+            }
+            else if(mat[r][c]>target)
+            {
+                high=mid-1;
+            }
+            else 
+            {
+                low=mid+1;
             }
         }
+
         return false;
+        
 
     }
 }
