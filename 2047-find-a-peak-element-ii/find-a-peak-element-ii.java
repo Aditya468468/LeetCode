910246f261247e4df68b4046a360ebef7f9aab07
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-30 18:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 116.2 MB (53.92%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -4,30 +4,30 @@ class Solution {
         int n=mat.length;
         int m=mat[0].length;
         int low=0;
-        int high=n-1;
+        int high=m-1;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
             int max=Integer.MIN_VALUE;
-            int maxCol=-1;
-            for(int i=0;i<m;i++)
+            int maxRow=-1;
+            for(int i=0;i<n;i++)
             {
-                if(mat[mid][i]>max)
+                if(mat[i][mid]>max)
                 {
-                    max=mat[mid][i];
-                    maxCol=i;
+                    max=mat[i][mid];
+                    maxRow=i;
                 }
                     
             }
-            int up=-1;
-            int down=-1;
-            if(mid-1>=0) up=mat[mid-1][maxCol];
-            if(mid+1<n) down=mat[mid+1][maxCol];
-            if(max>up && max>down)
+            int left=-1;
+            int right=-1;
+            if(mid-1>=0) left=mat[maxRow][mid-1];
+            if(mid+1<m) right=mat[maxRow][mid+1];
+            if(max>right && max>left)
             {
-                return new int[]{mid,maxCol};
+                return new int[]{maxRow,mid};
             }
-            else if(max>up) low=mid+1;
+            else if(max>left) low=mid+1;
             else high=mid-1;
         }
 
