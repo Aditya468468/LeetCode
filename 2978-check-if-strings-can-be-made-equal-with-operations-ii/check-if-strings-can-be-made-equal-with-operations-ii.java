9248cd28779a8a76ac3e85c546c5a38bc2a8fdf7
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-03-30 11:11:56 +0530

    Time: 5 ms (98.68%) | Memory: 48 MB (65.79%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,46 +1,28 @@
 class Solution {
-    public boolean checkStrings(String s1, String s2) 
-    {
-        //odd replaces odd, and even replaces even
-        // Lets Match s1 to s2.
-        char []nums1=s1.toCharArray();
-        char []nums2=s2.toCharArray();
-        int n=nums1.length;
-        HashMap<Character,Integer> even=new HashMap<>();
-        HashMap<Character,Integer> odd=new HashMap<>();
-        for(int i=0;i<n;i++)
-        {
-            char x=nums1[i];
-            if(i%2==0) even.put(x,even.getOrDefault(x,0)+1);
-            else odd.put(x,odd.getOrDefault(x,0)+1);
-        }
-        // Lets Match.
-        for(int i=0;i<n;i++)
-        {
-            // if(nums1[i]!=nums2[i])//Even if characters are equal at a position, they still “consume” one occurrence from the available pool.
-            // {
-                if(i%2==0)
-                {
-                    if(!even.containsKey(nums2[i]))
-                    {
-                        return false;
-                    }
-                    even.put(nums2[i],even.get(nums2[i])-1);
-                    if(even.get(nums2[i])==0) even.remove(nums2[i]);
-                }
-                else 
-                {
-                    if(!odd.containsKey(nums2[i]))
-                    {
-                        return false;
-                    }
-                    odd.put(nums2[i],odd.get(nums2[i])-1);
-                    if(odd.get(nums2[i])==0) odd.remove(nums2[i]);
-                }
-           // }
-        }
-
-        return true;
+    public boolean checkStrings(String s1, String s2) {
+        int n = s1.length();
         
+        int[] even1 = new int[26];
+        int[] odd1 = new int[26];
+        int[] even2 = new int[26];
+        int[] odd2 = new int[26];
+        
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                even1[s1.charAt(i) - 'a']++;
+                even2[s2.charAt(i) - 'a']++;
+            } else {
+                odd1[s1.charAt(i) - 'a']++;
+                odd2[s2.charAt(i) - 'a']++;
+            }
+        }
+        
+        for(int i = 0; i < 26; i++) {
+            if(even1[i] != even2[i] || odd1[i] != odd2[i]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
