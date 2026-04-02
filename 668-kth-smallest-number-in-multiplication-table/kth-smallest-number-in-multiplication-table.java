e6d611d10690d5486064ad180e87c0ed61c7be44
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-02 11:29:00 +0530

    Time: 14 ms (6.53%) | Memory: 42.2 MB (29.53%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
@@ -1,27 +1,37 @@
 class Solution {
-    public int count(int n,int m,long x)
+    // public int count(int n,int m,long x)
+    // {
+    //     int cnt=0;
+    //     for(int i=1;i<=n;i++)
+    //     {
+    //         int low=0;
+    //         int high=m-1;
+    //         while(low<=high)
+    //         {
+    //             int mid=low+(high-low)/2;
+    //             int value=i*(mid+1);
+    //             if(value<=x)
+    //             {
+    //                 low=mid+1;
+    //             }
+    //             else
+    //             {
+    //                 high=mid-1;
+    //             }
+    //         }
+    //         cnt+=low;
+    //     }
+    //     return cnt;
+    // }
+    public int count(int n, int m, long x)
     {
         int cnt=0;
         for(int i=1;i<=n;i++)
         {
-            int low=0;
-            int high=m-1;
-            while(low<=high)
-            {
-                int mid=low+(high-low)/2;
-                int value=i*(mid+1);
-                if(value<=x)
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high=mid-1;
-                }
-            }
-            cnt+=low;
+            cnt+=Math.min(m, x / i);
         }
-        return cnt;
+
+    return cnt;
     }
     public int findKthNumber(int n, int m, int k) 
     {
