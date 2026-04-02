7bbf8a8648b2fcd1bd795b42748e0e4af074f3c5
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-03 02:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (34.08%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) 
+    {
+        int A=nums[0];
+        int B=nums[1];
+        int C=nums[2];
+        if(A+B<=C || A+C<=B || B+C<=A) return "none";
+        if(A==B && B==C && A==C) return "equilateral";
+        else if(A!=B && B!=C && A!=C) return "scalene";
+        else return "isosceles";
+
+        
+    }
+};
