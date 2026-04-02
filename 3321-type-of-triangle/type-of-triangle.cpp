4cd85b871cedf9a0377f565e77c29fe3a0f87c0a
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-03 02:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (66.24%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -7,8 +7,9 @@ public:
         int C=nums[2];
         if(A+B<=C || A+C<=B || B+C<=A) return "none";
         if(A==B && B==C) return "equilateral";
-        else if(A!=B && B!=C && A!=C) return "scalene";
-        else return "isosceles";
+        else if((A==B && B!=C) || (A==C && B!=C) || (B==C && A!=B)) 
+            return "isosceles";
+        else return "scalene";
 
         
     }
