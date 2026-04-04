1cb6d6da86e38b5eff091550882ff9eef6c1cf01
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-05 01:04:53 +0530

    Time: 8 ms (60.96%) | Memory: 50.6 MB (97.19%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,37 +1,42 @@
-class Solution {
-
-    private boolean isPossible(int []nums,int limit,long k)
+class Solution 
+{   // Feasibilty.
+    private boolean canDivide(int[]nums,int limit,long k)
     {
         long sum=0;
         int n=nums.length;
         for(int i=0;i<n;i++)
         {
-            long value=(nums[i]+k-1)/k;
-            sum+=value;
+            sum+=(nums[i]+k-1)/k;
             if(sum>limit) return false;
         }
 
         return true;
+
     }
-    public int smallestDivisor(int[] nums, int threshold) 
+
+    public int smallestDivisor(int[] nums, int limit) 
     {
         long low=1;
         long high=Integer.MIN_VALUE;
-        for(int x:nums) if(x>high) high=x;
+        for(int x:nums)
+        {
+            if(high<x) high=x;
+        }
         while(low<=high)
         {
             long mid=low+(high-low)/2;
-            if(isPossible(nums,threshold,mid))
+            if(canDivide(nums,limit,mid))
             {
                 high=mid-1;
             }
-            else
+            else 
             {
                 low=mid+1;
             }
-        } 
+        }
 
         return (int)low;
-        
+
+       
     }
 }
