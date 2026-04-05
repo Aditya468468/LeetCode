5a8b5b3360d20176d8c865b48ab1c3a30eb3ba8f
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-05 12:43:51 +0530

    Time: 30 ms (49.38%) | Memory: 57.5 MB (86.42%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) 
+    {
+        int cols=encodedText.length()/rows;
+        char [][]matrix=new char[rows][cols];
+        int k=0;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                char x=encodedText.charAt(k++);
+                matrix[i][j]=x;
+            }
+        }
+        StringBuilder str=new StringBuilder();
+        //Like stairCase.
+        for(int j=0;j<cols;j++)
+        {
+            int x=j;
+            for(int i=0;i<rows && x<cols;i++)
+            {
+                char c=matrix[i][x];
+                x++;
+                str.append(c);
+            }
+        }
+        
+        int end=str.length()-1;
+        while(end>=0 && str.charAt(end)==' ')
+        {
+            end--;
+        }
+        return str.substring(0,end+1).toString();
+
+        
+    }
+}
