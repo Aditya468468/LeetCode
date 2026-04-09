7be996a0e6e031610c8dba1df5e97f749a5008bf
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-09 09:40:47 +0530

    Time: 2 ms (71.83%) | Memory: 44.8 MB (49.76%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,31 +1,28 @@
 class Solution 
 {
-    private void generate(int n,List<String> list,String s,int open,int close)
+    private void generate(int n,List<String> ans,String s,int open,int close)
     {
         if(open+close==2*n && open==close)
         {
-            list.add(s);
+            ans.add(s);
             return;
         }
         if(open<n)
         {
-            //s=s+'(';
-            generate(n,list,s+'(',open+1,close);
+            generate(n,ans,s+'(',open+1,close);
         }
         if(close<open)
         {
-            //s+=')';// this will change the state in current fun space.
-            //Wrong we need to keep this as it is
-            generate(n,list,s+')',open,close+1); // and pass new to next fun call.
-            // Else i need to redo it.
-            // (_--> lets say (( okay now,
-            //for the next if block-> (()->3 noo..it should be -> ()
+            generate(n,ans,s+')',open,close+1);
         }
     }
     public List<String> generateParenthesis(int n) 
     {
-        List<String> list=new ArrayList<>();
-        generate(n,list,"",0,0);
-        return list;
+        List<String> ans=new ArrayList<>();
+        generate(n,ans,"",0,0);
+
+        return ans;
+
+        
     }
 }
