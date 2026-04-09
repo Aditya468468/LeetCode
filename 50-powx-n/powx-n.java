f26372a61452fb92acfc9d39e1005977be568d8a
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-09 10:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (50.86%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,15 +1,15 @@
-class Solution 
-{
-    private double helper(double x, long n)
+class Solution {
+    //Fast Exponential Method
+    private double pow(double x,long n)
     {
         if(n==0) return 1;
         if(n%2==0)
         {
-            return helper(x*x,n/2);
+            return pow(x*x,n/2);
         }
-        else 
+        else
         {
-            return x*helper(x,n-1);
+            return x*pow(x,n-1);
         }
     }
     public double myPow(double x, int n) 
@@ -20,8 +20,8 @@ class Solution
             x=1/x;
             N=(-1L)*N;
         }
-        return helper(x,N);
-
-          
+        
+        return pow(x,N);
+        
     }
 }
