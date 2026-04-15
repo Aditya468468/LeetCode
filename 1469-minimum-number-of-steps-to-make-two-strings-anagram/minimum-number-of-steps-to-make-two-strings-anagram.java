dbb54c2472eaa3dff08d56980eb48351187515bf
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-15 17:52:51 +0530

    Time: 49 ms (23.11%) | Memory: 47.7 MB (31.77%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -5,35 +5,26 @@ class Solution {
         //length will be same only naa.
         int n=s.length();
         if(n!=t.length()) return -1;
-        Map<Character,Integer> map1=new HashMap<>();
-        Map<Character,Integer> map2=new HashMap<>();
+        Map<Character,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++)
         {
             char x=s.charAt(i);
-            char y=t.charAt(i);
-            map1.put(x,map1.getOrDefault(x,0)+1);
-            map2.put(y,map2.getOrDefault(y,0)+1);
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(int i=0;i<n;i++)
+        {
+            char x=t.charAt(i);
+            map.put(x,map.getOrDefault(x,0)-1);
         }
         int need=0;
         
-        for(Map.Entry<Character,Integer> it:map1.entrySet())
+        for(Map.Entry<Character,Integer> it:map.entrySet())
         {
-            char key=it.getKey();
             int value=it.getValue();
-
-            if(map2.containsKey(key))
-            {
-                if(map2.get(key)<value)
-                {
-                    need+=value-map2.get(key); 
-                }
-                //if enough is there..no need to do anything at all->else part
-            }
-            else
+            if(value>0)
             {
                 need+=value;
             }
-
         }
 
         return need;
