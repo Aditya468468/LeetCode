d578db18d9455c0a170cd01eb051c6f3dc80dc99
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-18 12:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (79.63%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.java
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.java
@@ -1,30 +1,19 @@
 class Solution {
 
-    static final long MOD=1_000_000_007;
-    public long pow(long x,int n) // x^n
+   long MOD=1_000_000_007;
+
+    private long moves(long x,long n)
     {
         if(n==0) return 1;
-        if(n%2==0)
-        {
-            return (pow((x*x)%MOD,n/2))%MOD;
-        }
-        else 
-        {
-            return (x*pow(x,n-1))%MOD;
-        }
-
+        if(n%2==0) return moves((x*x)%MOD,n/2)%MOD;
+        else return (x*moves(x,n-1))%MOD;
     }
     public int monkeyMove(int n) 
     {
-        // Instead of Calculating -> Atleast one collison.(More also counted).
-        //Lets find Total=(Collison)+(Safe);
-        //Safe-> When all goes CW or All Go ACW-> 2 ways.
-        // And Total-> Each vertex has a choice of ACW or CW-> 2^n
-        // Sooo-> Collison=Total-Safe-> 2^n-2;
-
-        return (int)((pow(2, n)-2+MOD)%MOD);
-
-
+    
+        //return (int)(moves(2,n)-2);
+        return (int)((moves(2,n)-2+MOD)%MOD);
+        
         
     }
 }
