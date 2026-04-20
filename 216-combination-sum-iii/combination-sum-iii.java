b72dfd02768611f528ed3ba21b12fd13f57f971c
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-20 21:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (51.71%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,30 +1,28 @@
 class Solution 
 {
-    public void generate(int idx,List<List<Integer>> ans,List<Integer> list,int k,int n)
+    public void comb(int idx,List<List<Integer>> ans,List<Integer> list,int k,int sum)
     {
-        if(n==0 && list.size()==k)
+        if(sum==0 && list.size()==k)
         {
             ans.add(new ArrayList<>(list));
             return;
         }
-        if(n<=0 || list.size()>k || idx>9) return; //prune.
-        //Take it.
-        list.add(idx);
-        generate(idx+1,ans,list,k,n-idx);
-        list.remove(list.size()-1);
-        //Not take.
-        generate(idx+1,ans,list,k,n);
-
+        if(sum<=0 || list.size()>k || idx>9) return;
+        //For-loop recursion 
+        for(int i=idx;i<=9;i++)
+        {
+            if(i>sum) break;
+            list.add(i);
+            comb(i+1,ans,list,k,sum-i);
+            list.remove(list.size()-1);
+        }
     }
     public List<List<Integer>> combinationSum3(int k, int n) 
     {
-        // 1,2,3,4,5...9 is already sorted with no duplicates.
-        //-> soo same comb will not form.
+
         List<List<Integer>> ans=new ArrayList<>();
-        generate(1,ans,new ArrayList<>(),k,n);
+        comb(1,ans,new ArrayList<>(),k,n);;
 
         return ans;
-
-       
     }
 }
