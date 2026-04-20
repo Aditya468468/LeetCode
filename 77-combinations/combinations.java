fb393ef8c4f5f60dd0e0ec9a1cb1545e83e486c9
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-20 22:25:48 +0530

    Time: 23 ms (15.49%) | Memory: 98.3 MB (16.73%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,26 +1,24 @@
-class Solution {
-    public void comb(int idx,List<List<Integer>> ans,List<Integer> list,int n,int k)
+class Solution 
+{
+    public void comb(int idx, int n, int k, List<List<Integer>> ans, List<Integer> list)
     {
         if(list.size()==k)
         {
             ans.add(new ArrayList<>(list));
             return;
         }
-        //Pruning-> GPT Helped,For Pruning.
-        if(n - idx + 1 < k - list.size()) return;
-        if(idx>n) return;
-        //Take it.
+        if(list.size()>k || idx>n) return;
+        //Take it
         list.add(idx);
-        comb(idx+1,ans,list,n,k);
-        list.remove(list.size()-1); //Backtrack/Undo
-        //Not Take.
-        comb(idx+1,ans,list,n,k);
+        comb(idx+1,n,k,ans,list);
+        list.remove(list.size()-1);
+        //Not take
+        comb(idx+1,n,k,ans,list);
     }
     public List<List<Integer>> combine(int n, int k) 
     {
         List<List<Integer>> ans=new ArrayList<>();
-        comb(1,ans,new ArrayList<>(),n,k);
-
+        comb(1,n,k,ans,new ArrayList<>());
         return ans;
 
         
