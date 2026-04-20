a1bf8f63c6be48f82753948ecaee4cf199979fcd
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-20 15:48:31 +0530

    Time: 2 ms (95.37%) | Memory: 45.7 MB (70.50%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,31 +1,30 @@
-class Solution 
-{
-    public void comb(int idx,int[]nums,List<List<Integer>> ans,List<Integer> list,int target)
+class Solution {
+
+    public void count(int idx,int[]nums,List<List<Integer>> ans,List<Integer> list,int sum)
     {
-        if(target==0)
+        if(sum==0)
         {
             ans.add(new ArrayList<>(list));
             return;
         }
-        if(target<0 || idx==nums.length)
+        if(sum<0 || idx==nums.length)
         {
             return;
         }
-        //Lets take and stay.
+        //Stay
         list.add(nums[idx]);
-        comb(idx,nums,ans,list,target-nums[idx]);
-        list.remove(list.size()-1);//backtrack.
-
-        //Lets Not take nd Move.
-        comb(idx+1,nums,ans,list,target);
-
+        count(idx,nums,ans,list,sum-nums[idx]);
+        //BackTrack
+        list.remove(list.size()-1);
+        //Move;
+        count(idx+1,nums,ans,list,sum);
 
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) 
     {
         List<List<Integer>> ans=new ArrayList<>();
-        comb(0,candidates,ans,new ArrayList<>(),target);
-
-        return ans;   
+        count(0,candidates,ans,new ArrayList<>(),target);
+        return ans;
+        
     }
 }
