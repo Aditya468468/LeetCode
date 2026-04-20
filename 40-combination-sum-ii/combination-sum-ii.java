fc6ad921a3e2ce68637aa7ea1573dcba0defbd80
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-20 17:20:27 +0530

    Time: 5 ms (99.56%) | Memory: 45.3 MB (42.42%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -13,7 +13,7 @@ class Solution {
         for(int i=idx;i<nums.length;i++)
         {
             if(i>idx && nums[i]==nums[i-1]) continue;
-            if(nums[i]>target); //Pruning!!
+            if(nums[i]>target) break; //Pruning!!
             list.add(nums[i]);
             comb(i+1,nums,ans,list,target-nums[i]);
             list.remove(list.size()-1);
