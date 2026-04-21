6be374f8ba6567cd2bf50dcb15a24ffc3080b518
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-21 14:23:54 +0530

    Time: 2 ms (99.58%) | Memory: 44.9 MB (89.40%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,34 +1,27 @@
 class Solution 
 {
-    private void generate(int idx,int[]nums,List<List<Integer>> ans,List<Integer> list)
+    private void generate(int idx,int []nums,List<List<Integer>> ans,List<Integer> list)
     {
-        if(idx==nums.length)
-        {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        //Take it.
-        list.add(nums[idx]);
-        generate(idx+1,nums,ans,list);
-        //Backtrack.
-        list.remove(list.size()-1);
-        //Not take any of its occ in that rec level.
-        int i=idx+1;
-        while(i<nums.length && nums[idx]==nums[i])
-        {
-            i++;
-        }
-        generate(i,nums,ans,list);
-    }
+        ans.add(new ArrayList<>(list));//Every state is Valid Subset.
 
-    
+        for(int i=idx;i<nums.length;i++)
+        {
+            if(i>idx && nums[i-1]==nums[i]) continue;
+            //Skiping Duplicates On Same Recursion Level
+            list.add(nums[i]);
+            generate(i+1,nums,ans,list);
+            list.remove(list.size()-1);
+        }
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) 
     {
-        Arrays.sort(nums);// Imp to skip Duplicates.
+        Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
         generate(0,nums,ans,new ArrayList<>());
-
         return ans;
-        
+      
     }
 }
+
+
+
