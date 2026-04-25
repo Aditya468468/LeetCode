c9e4b06e1e92be9e3bd87f993780b30a8c0b663c
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-26 01:57:02 +0530

    Time: 11 ms (37.64%) | Memory: 179.7 MB (6.36%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) 
     {
-        HashMap<Integer,int[]> row=new HashMap<>();
-        HashMap<Integer,int[]> col=new HashMap<>();
         int n=grid.length;
         int m=grid[0].length;
+        int []row=new int[n];
+        int []col=new int[m];
         for(int i=0;i<n;i++)
         {
             int ones=0;
@@ -14,7 +14,7 @@ class Solution {
                 if(grid[i][j]==0) zeros++;
                 else ones++;
             }
-            row.put(i,new int[]{zeros,ones});
+            row[i]=ones-zeros;
         }
         for(int i=0;i<m;i++)
         {
@@ -25,21 +25,16 @@ class Solution {
                 if(grid[j][i]==0) zeros++;
                 else ones++;
             }
-            col.put(i,new int[]{zeros,ones});
+            col[i]=ones-zeros;
         }
         int [][]diff=new int[n][m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                int[] rowData=row.get(i);
-                int[] colData=col.get(j);
-                int onesRow=rowData[1];
-                int zerosRow=rowData[0];
-                int onesCol=colData[1];
-                int zerosCol=colData[0];
-                diff[i][j]=onesRow+onesCol-zerosRow-zerosCol;
-
+                int effRow=row[i];
+                int effCol=col[j];
+                diff[i][j]=effRow+effCol;
             }
         }
 
