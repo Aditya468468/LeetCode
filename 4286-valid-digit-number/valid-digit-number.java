4a41178d3b7f75336840c99ca3d2ad45aaec365b
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-25 22:49:05 +0530

    Time: 1 ms (99.64%) | Memory: 42.6 MB (82.37%) - LeetSync

--- 4286-valid-digit-number/valid-digit-number.java
+++ 4286-valid-digit-number/valid-digit-number.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean validDigit(int n, int x) 
     {
-       int ld=-1;
+        int digits=(int)Math.log10(n);
+        int first=(int)(n / Math.pow(10, digits));
+        if(first==x) return false;
+
        boolean valid=false;
        while(n!=0)
        {
-            ld=n%10;
+            int ld=n%10;
             if(!valid && ld==x)
             {
                 valid=true;
             }
             n/=10;
        }
-        return valid && ld!=x;
+        return valid;
         
         
     }
