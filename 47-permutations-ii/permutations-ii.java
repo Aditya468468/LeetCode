e671acf3f8ef424f91df08d43ec4de8694dd3ce9
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-04-29 18:38:05 +0530

    Time: 3 ms (88.70%) | Memory: 46.6 MB (61.65%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,34 +1,33 @@
 class Solution 
 {
-    private void generate(int []nums,boolean[]used,List<List<Integer>> ans,List<Integer> list)
+    private void perm(int[]nums,boolean []used,List<List<Integer>> ans,List<Integer> list)
     {
         if(list.size()==nums.length)
         {
-            ans.add(new ArrayList<Integer>(list));
+            ans.add(new ArrayList<>(list));
             return;
         }
+        //Lets Explore.
         for(int i=0;i<nums.length;i++)
         {
-            
             if(used[i]==true) continue;
-            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+            if(i>0 && nums[i-1]==nums[i] && !used[i-1]) continue;
             used[i]=true;
             list.add(nums[i]);
-            generate(nums,used,ans,list);
-            used[i]=false;
+            perm(nums,used,ans,list);
             list.remove(list.size()-1);
-
+            used[i]=false;
 
         }
     }
     public List<List<Integer>> permuteUnique(int[] nums) 
     {
         Arrays.sort(nums);
-        List<List<Integer>> ans =new ArrayList<>();
-        generate(nums,new boolean[nums.length],ans,new ArrayList<Integer>());
+        List<List<Integer>> ans=new ArrayList<>();
+        perm(nums,new boolean[nums.length],ans,new ArrayList<>());
 
         return ans;
-        
+
         
     }
 }
