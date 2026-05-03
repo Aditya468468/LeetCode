7293d70caaaa0600523e6ac74caee12bcc57ed09
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-03 13:33:57 +0530

    Time: 294 ms (49.07%) | Memory: 93.4 MB (48.52%) - LeetSync

--- 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
@@ -1,46 +1,42 @@
-class Solution 
-{
-    private boolean isPrime(int n)
+class Solution {
+    private boolean isPrime(int x )
     {
-        if(n<=1) return false;
-        for(int i=2;i*i<=n;i++)
+        if(x < 2) return false;
+        for(int i=2;i*i<=x;i++)
         {
-            if(n%i==0)
-            {
-                return false;
-            }
-
+            if(x%i==0) return false;
         }
         return true;
     }
     public int minOperations(int[] nums) 
     {
-        int n=nums.length;
         int ops=0;
-        //even-Prime, odd-> NotPrime
+        int n=nums.length;
         for(int i=0;i<n;i++)
         {
             if(i%2==0)
             {
-                while(!isPrime(nums[i]))
-                {
-                    nums[i]++;
-                    ops++;
-                }
-            }
-            else 
-            {
-                 while(isPrime(nums[i]))
-                {
-                    nums[i]++;
-                    ops++;
-                }
                 
+                    while(!isPrime(nums[i]))
+                    {
+                        nums[i]++;
+                        ops++;
+                    }
+                
+            }
+            else
+            {
+            
+                    while(isPrime(nums[i]))
+                    {
+                        nums[i]++;
+                        ops++;
+                    }
+
             }
         }
 
         return ops;
-
         
     }
 }
