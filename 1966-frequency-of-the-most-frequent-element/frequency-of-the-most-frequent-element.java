1e135b09d48ce68760fc7a32756310d54df32bf4
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-14 12:15:18 +0530

    Time: 33 ms (92.85%) | Memory: 94.8 MB (46.48%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,24 +1,30 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) 
     {
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Nearer will Compute first.
         int n=nums.length;
-        int maxCnt=Integer.MIN_VALUE;
+        int maxLen=0;
         int left=0;
         long sum=0;
         for(int right=0;right<n;right++)
         {
+
             sum+=nums[right];
-            while((long)nums[right]*(right-left+1)-sum>k)
+            // In L len -> All elements same will Have Sum= (len)*MaxElem.
+            //As only inc is possible we need to come up till MAX of Array.
+            while((long)nums[right]*(right-left+1)>sum+k)
             {
                 sum-=nums[left];
                 left++;
             }
             int cnt=right-left+1;
-            if(cnt>maxCnt) maxCnt=cnt;
-        }
-        
-        return maxCnt;
+            if (cnt>maxLen) maxLen=cnt;
 
+        }
+
+        return maxLen;
+
+
+        
     }
 }
