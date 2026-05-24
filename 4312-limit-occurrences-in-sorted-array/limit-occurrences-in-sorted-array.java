cedb0a71541bf415cf10f49f7d1b2bb4cbc92d00
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-24 10:20:29 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (96.73%) - LeetSync

--- 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.java
+++ 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.java
@@ -1,35 +1,29 @@
 class Solution {
     public int[] limitOccurrences(int[] nums, int k) 
     {
-        ArrayList<Integer> list=new ArrayList<>();
-        list.add(nums[0]);
         int n=nums.length;
+        int prev=nums[0];
+        int idx=1;
         int cnt=1;
-        for(int i=1;i<nums.length;i++)
+        for(int i=1;i<n;i++)
         {
-            if(nums[i-1]==nums[i])
-            {
-                if(cnt<k)
-                {
-                    cnt++;
-                    list.add(nums[i]);
-                }
-            }
-            else if(nums[i-1]!=nums[i])
+            if(nums[i]!=prev)
             {
                 cnt=1;
-                list.add(nums[i]);
+                prev=nums[i];
+            }
+            else 
+            {
+                cnt++;
+            }
+            if(cnt<=k)
+            {
+                nums[idx++]=nums[i];
             }
         }
-        int[]ans=new int[list.size()];
-        int idx=0;
-        for(int x:list)
-        {
-            ans[idx++]=x;
-        }
 
-        return ans;
-
+        return Arrays.copyOf(nums, idx);
+        
         
     }
 }
