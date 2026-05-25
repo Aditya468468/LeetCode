2db3defac3a5dbc9bfe137b8951fa2082d67373e
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-26 01:21:43 +0530

    Time: 48 ms (5.07%) | Memory: 55.7 MB (5.37%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String convert(String s, int numRows) 
+    {
+        int n=s.length();
+        if(numRows<=1 || n<=1) return s; //Asked GPT
+        char[][] grid=new char[numRows][2*(n-1)];
+        char []nums=s.toCharArray();
+        boolean down=true;
+        int col=0;
+        int row=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(row==numRows)
+            {
+                row=numRows-2;
+                col++;
+                down=false;
+            }
+            if(row<0)
+            {
+                row=1;
+                down=true;
+
+            }
+            if(down)
+            {
+                grid[row][col]=nums[i];
+                row++;
+            }
+            if(!down)
+            {
+                grid[row][col]=nums[i];
+                row--;
+                col++;
+
+            }
+           
+        }
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<numRows;i++)
+        {
+            for(int j=0;j<s.length();j++)
+            {
+                if(grid[i][j]!='\u0000')
+                {
+                    str.append(grid[i][j]);
+                }
+            }
+        }
+
+        return str.toString();
+    }
+}
