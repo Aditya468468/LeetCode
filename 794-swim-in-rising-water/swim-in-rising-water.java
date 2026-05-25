d9d4dd9c0a309637603b6e505f660b60d5c80fe6
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-25 19:57:34 +0530

    Time: 3 ms (95.98%) | Memory: 45.9 MB (94.65%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    public boolean canReach(int[][]grid,int i,int j, boolean [][]visited,int k)
+    {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length)
+        {
+            return false; // Boundaries 
+        }
+        if(visited[i][j]==true)
+        {
+            return false; // Save from self loop
+        }
+        if(grid[i][j]>k)
+        {
+            return false; // Water level is High
+        }
+        if(i==grid.length-1 && j==grid[0].length-1)
+        {
+            return true;
+        }
+        visited[i][j]=true;
+        if(canReach(grid,i+1,j,visited,k)) return true;
+        if(canReach(grid,i-1,j,visited,k)) return true;
+        if(canReach(grid,i,j+1,visited,k)) return true;
+        if(canReach(grid,i,j-1,visited,k)) return true;
+
+
+        return false;
+    
+    }
+    public int swimInWater(int[][] grid) 
+    {
+        int n=grid.length;
+        int m=grid[0].length;
+        int low=0;
+        int high=Integer.MIN_VALUE;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]>high)
+                {
+                    high=grid[i][j];
+                }
+            }
+        }
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(canReach(grid,0,0,new boolean[n][m],mid))
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+
+        return low;
+
+        
+    }
+}
