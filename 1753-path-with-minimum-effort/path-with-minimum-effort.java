d314dee5c0bbca0fb30dc79a006c39b7372c1e07
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-25 21:29:07 +0530

    Time: 47 ms (43.73%) | Memory: 48.1 MB (25.03%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    private boolean canReach(int[][]heights,int i,int j, boolean visited[][],int k,int prev)
+    {
+        if(i<0 || i>=heights.length || j<0 || j>=heights[0].length) return false;
+        if(visited[i][j]) return false;
+        if(Math.abs(heights[i][j]-prev)>k) return false;
+        if(i==heights.length-1 && j==heights[0].length-1) return true;
+        visited[i][j]=true;
+        if(canReach(heights,i+1,j,visited,k,heights[i][j])) return true;
+        if(canReach(heights,i-1,j,visited,k,heights[i][j])) return true;
+        if(canReach(heights,i,j+1,visited,k,heights[i][j])) return true;
+        if(canReach(heights,i,j-1,visited,k,heights[i][j])) return true;
+
+        return false;   
+    }
+    public int minimumEffortPath(int[][] heights) 
+    {
+        int n=heights.length;
+        int m=heights[0].length;
+        // int low=Math.min(Math.abs(heights[0][0]-heights[1][0]),Math.abs(heights[0][0]-heights[0][1]));
+        int low=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(heights[i][j]<min) min=heights[i][j];
+                if(heights[i][j]>max) max=heights[i][j];
+            }
+        }
+        int high=max-min;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(canReach(heights,0,0,new boolean[n][m],mid,heights[0][0]))
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+
+        return low;
+        
+    }
+}
