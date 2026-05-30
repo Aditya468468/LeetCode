de6e2b5ba6875d70c73b4ed93783296286e74b71
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-05-30 12:59:21 +0530

    Time: 49 ms (92.31%) | Memory: 107.4 MB (5.49%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,4 +1,30 @@
-class Solution {
+class Solution 
+{
+    public void countSort(ArrayList<Integer> list)
+    {
+        int max=Integer.MIN_VALUE;
+        for(int x:list)
+        {
+            if(x>max) max=x;
+        }
+        int []freq=new int[max+1];
+        for(int i=0;i<list.size();i++)
+        {
+            freq[list.get(i)]++;
+        }
+        int idx=0;
+        for(int i=0;i<freq.length;i++)
+        {
+            while(freq[i]!=0)
+            {
+             
+                list.set(idx,i);
+                freq[i]--;
+                idx++;
+            }
+            
+        }
+    }
     public int rangeSum(int[] nums, int n, int left, int right) 
     {
         int MOD=1000_000_007;
@@ -15,7 +41,9 @@ class Solution {
             }
 
         }
-        Collections.sort(list);
+        //Collections.sort(list);
+        //Lets COUNT_SORT.
+        countSort(list);
         int totalSum=0;
         for(int i=left-1;i<right;i++)
         {
