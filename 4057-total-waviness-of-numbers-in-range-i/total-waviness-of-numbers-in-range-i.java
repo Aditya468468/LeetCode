6ca6fedf8fe20499b4f06541b73153ebe20cc9aa
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-04 07:33:30 +0530

    Time: 50 ms (9.29%) | Memory: 46.5 MB (52.04%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -5,7 +5,7 @@ class Solution
         if(list.size()<3) return 0;
         //Give 1st and Last cant be peak/Valley so we can skip EDGE cases.
         int cnt=0;
-        for(int i=1;i<list.size()-1;i++)
+        for(int i=list.size()-2;i>=1;i--)
         {
             if(list.get(i-1)<list.get(i) && list.get(i)>list.get(i+1))
             {
@@ -27,7 +27,7 @@ class Solution
             x/=10;
         }
 
-        Collections.reverse(list);
+       // Collections.reverse(list);
         return list;
 
     }
