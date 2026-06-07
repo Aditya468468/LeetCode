6b9b6c1b162f50f03e569cadb75011b9ef649145
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-07 14:33:28 +0530

    Time: 1 ms (86.45%) | Memory: 44.7 MB (45.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,34 +1,37 @@
 class Solution 
 {
-    private void generate(int n,List<String> ans,StringBuilder str,int open,int close)
+    private void generate(int n,int idx,int open, int close,StringBuilder str,List<String> list)
     {
-        if(open+close==2*n && open==close)
+        if(idx==2*n)
         {
-            ans.add(new String(str));
+            list.add(new StringBuilder(str).toString());
             return;
         }
+        // There can be 2 options, At idx,
+        // But if(open>close) -> there should be opening for closing.
+        // if(open<n)-> there cant be more then 2 opening
+        //Lets See for Open at idx,
         if(open<n)
         {
             str.append('(');
-            generate(n,ans,str,open+1,close);
-            //Backtrack
+            generate(n,idx+1,open+1,close,str,list);
+            //We goo its depth.. Then we back track.
             str.deleteCharAt(str.length()-1);
         }
+        // Can that idx have closing;
         if(close<open)
         {
             str.append(')');
-            generate(n,ans,str,open,close+1);
-            //Backtrack.
-            str.deleteCharAt(str.length()-1);
+            generate(n,idx+1, open, close+1,str,list);
+            str.deleteCharAt(str.length()-1);//Backtrack.
         }
+
     }
     public List<String> generateParenthesis(int n) 
     {
-        List<String> ans=new ArrayList<>();
-        generate(n,ans,new StringBuilder(),0,0);
+        List<String> list=new ArrayList<>();
+        generate(n,0,0,0,new StringBuilder(),list);
 
-        return ans;
-
-        
+        return list;    
     }
 }
