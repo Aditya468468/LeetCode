dcb27073eb1e0bcc9e301aa243b75391ec2c56a1
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-07 13:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (10.65%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,21 +1,28 @@
-class Solution {
-
-    private double pow(double x,long N)
+class Solution 
+{
+    private double pow(double x,long n)
     {
-        if(N==0) return 1; // Base Case
-        if(N%2==0) return pow(x*x,N/2);
-        else return x*pow(x,N-1);
-    }
-    public double myPow(double x, int n) 
-    {
-        long N=n;
-        if(N<0)
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n%2==0)
         {
-            x=1/x;
-            N=(-1)*N;
+            return pow(x*x,n/2);
         }
-        return pow(x,N);
-
-        
+        else
+        {
+            return x*pow(x,n-1);
+        }
+    }
+    public double myPow(double x, int N) 
+    {
+        long n=N;
+        if(n==0) return 1;
+        if(n<0)
+        {
+            n=(-1)*n;
+            x=1/x;
+        }
+        return pow(x,n);
+         
     }
 }
