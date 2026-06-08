dd5333284bdbd55e3a5bfaa35b7bf7fb18da6473
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-08 20:17:59 +0530

    Time: 2 ms (99.65%) | Memory: 44.8 MB (87.40%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,27 +1,32 @@
 class Solution 
 {
-    private void generate(int idx,int []nums,List<List<Integer>> ans,List<Integer> list)
+    private void generate(int idx,int[]nums,List<Integer> list,List<List<Integer>> ans)
     {
-        ans.add(new ArrayList<>(list));//Every state is Valid Subset.
-
-        for(int i=idx;i<nums.length;i++)
+        if(idx==nums.length)
         {
-            if(i>idx && nums[i-1]==nums[i]) continue;
-            //Skiping Duplicates On Same Recursion Level
-            list.add(nums[i]);
-            generate(i+1,nums,ans,list);
-            list.remove(list.size()-1);
+            ans.add(new ArrayList<>(list));
+            return;
         }
+        // Take it.
+        list.add(nums[idx]);
+        generate(idx+1,nums,list,ans);
+        list.remove(list.size()-1);
+        //Not Take it, So We wont take Any occ at that level.
+        while(idx<nums.length-1 && nums[idx]==nums[idx+1])
+        {
+            idx++;
+        }
+        generate(idx+1,nums,list,ans);
+
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) 
     {
         Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
-        generate(0,nums,ans,new ArrayList<>());
+        generate(0,nums,new ArrayList<>(), ans);
+
         return ans;
-      
+
+        
     }
 }
-
-
-
