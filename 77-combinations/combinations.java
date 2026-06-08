2dd02da6706a87575e059975d1ac2daea20de34e
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-08 16:24:28 +0530

    Time: 18 ms (81.28%) | Memory: 98.2 MB (27.36%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,26 +1,25 @@
 class Solution 
 {
-    public void comb(int idx, int n, int k, List<List<Integer>> ans, List<Integer> list)
+    private void generate(int idx,int n,int k,List<List<Integer>> ans,List<Integer> list)
     {
         if(list.size()==k)
         {
             ans.add(new ArrayList<>(list));
             return;
         }
-        if(list.size()>k || idx>n) return;
-        //for-loop
         for(int i=idx;i<=n;i++)
         {
-
             list.add(i);
-            comb(i+1,n,k,ans,list);
+            generate(i+1,n,k,ans,list);
             list.remove(list.size()-1);
         }
     }
     public List<List<Integer>> combine(int n, int k) 
     {
-        List<List<Integer>> ans=new ArrayList<>();
-        comb(1,n,k,ans,new ArrayList<>());
+
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(1,n,k,ans,new ArrayList<>());
+
         return ans;
 
         
