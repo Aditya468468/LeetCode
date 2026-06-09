7f6886f6bc097df6b8bf533486b2dd92d16b7ca8
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-09 09:05:38 +0530

    Time: 1 ms (87.76%) | Memory: 45.3 MB (88.85%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,30 +1,31 @@
 class Solution 
 {
-    private void perm(int[]nums,boolean[]used,List<List<Integer>> ans, List<Integer> list)
+    private void perm(int[]nums,boolean[]used,List<Integer> list,List<List<Integer>> ans)
     {
         if(list.size()==nums.length)
         {
             ans.add(new ArrayList<>(list));
             return;
         }
-        //
         for(int i=0;i<nums.length;i++)
         {
-            if(used[i]==true) continue;
+            if(used[i]) continue; //If the element has be used in parent trees,we cant use it.
+            //mark
             used[i]=true;
             list.add(nums[i]);
-            perm(nums,used,ans,list);
+            perm(nums,used,list,ans);
+            //Back-track.
             list.remove(list.size()-1);
             used[i]=false;
         }
+
     }
     public List<List<Integer>> permute(int[] nums) 
     {
         List<List<Integer>> ans=new ArrayList<>();
-        perm(nums,new boolean[nums.length],ans,new ArrayList<>());
+        perm(nums,new boolean[nums.length],new ArrayList<>(),ans);
 
         return ans;
-
         
     }
 }
