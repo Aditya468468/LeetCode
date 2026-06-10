9c58753983f61aa9ae83a11bbb4b47e8c04071c4
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-10 10:55:04 +0530

    Time: 1 ms (56.45%) | Memory: 43.5 MB (25.00%) - LeetSync

---
+++ 1976-splitting-a-string-into-descending-consecutive-values/splitting-a-string-into-descending-consecutive-values.java
@@ -0,0 +1,55 @@
+class Solution 
+{
+    public long toNumber(String str)
+    {
+        int i=0;
+        long num=0;
+        while(i<str.length() && str.charAt(i)=='0')
+        {
+            i++;
+        }
+        while(i<str.length()) // if(i==str.length())-> 00000--> 0
+        {
+            //eg:-985
+            num=(num*10)+(str.charAt(i)-'0');
+            //0*10+9=9, 9*10+8=98, 98*100+5=985
+            i++;
+        }
+
+        return num;
+
+    }
+    public boolean canSplit(String str,long prevd)
+    {
+        if(prevd==-1) return true; // First split
+        long num=toNumber(str);
+        if(prevd-num==1) return true;
+        return false;
+
+    }
+    public boolean check(int idx,String s, long prevd,int cnt)
+    {
+        if(idx==s.length())
+        {
+            return cnt>=2;
+        }
+        for(int i=idx;i<s.length();i++)
+        {
+            String str=s.substring(idx,i+1);
+            if(canSplit(str,prevd))
+            {
+                 if(check(i+1,s,toNumber(str),cnt+1))
+                 {
+                    return true;
+                 }
+            }
+        }
+
+        return false;
+    }
+    public boolean splitString(String s) 
+    {
+        return check(0,s,-1,0);
+
+    }
+}
