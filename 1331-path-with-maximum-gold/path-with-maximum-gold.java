152a967f377343a10784bfbd4855e47312b63916
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-14 15:15:59 +0530

    Time: 62 ms (72.20%) | Memory: 42.9 MB (71.54%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,24 +1,21 @@
 class Solution 
 {
     int maxGold=0;
-
-    private void collect(int[][]grid,boolean[][]visited,int i,int j,int gold)
+    private void collect(int i,int j,int[][]grid,int gold)
     {
-        maxGold=Math.max(maxGold,gold); //Update
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length)
-        {
-            return;
-        }
+        maxGold=Math.max(maxGold,gold);
+        int n=grid.length;
+        int m=grid[0].length;
+        if(i<0 || i>=n || j<0 || j>=m) return;
         if(grid[i][j]==0) return;
-        if(visited[i][j]==true) return;
-        visited[i][j]=true;
-        collect(grid,visited,i-1,j,gold+grid[i][j]); //UP
-        collect(grid,visited,i+1,j,gold+grid[i][j]); //DOWN
-        collect(grid,visited,i,j-1,gold+grid[i][j]); //LEFT
-        collect(grid,visited,i,j+1,gold+grid[i][j]); //RIGHT
-
-        visited[i][j]=false; //Backtrack.
-        
+        int currGold=grid[i][j];
+        grid[i][j]=0;
+        collect(i-1,j,grid,gold+currGold);
+        collect(i+1,j,grid,gold+currGold);
+        collect(i,j-1,grid,gold+currGold);
+        collect(i,j+1,grid,gold+currGold);
+        grid[i][j]=currGold;
+        return;
     }
     public int getMaximumGold(int[][] grid) 
     {
@@ -30,13 +27,12 @@ class Solution
             {
                 if(grid[i][j]!=0)
                 {
-                    collect(grid,new boolean[grid.length][grid[0].length],i,j,0);
-                    //Start exploring the path.
+                    collect(i,j,grid,0);
                 }
-
             }
         }
 
         return maxGold;
+
     }
 }
