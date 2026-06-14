30630b512609064f64588a8d1585c6bc8664e2e5
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-14 12:26:59 +0530

    Time: 150 ms (38.97%) | Memory: 42.7 MB (87.08%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,40 @@
 class Solution 
 {
-    public boolean search(int idx,String word,char[][]board,int i,int j, boolean[][]visited)
+    private boolean explore(int idx,char[][]board,String word,boolean[][]visited,int i,int j)
     {
         int n=board.length;
         int m=board[0].length;
         if(i<0 || i>=n || j<0 || j>=m) return false;
-        if(visited[i][j]==true || board[i][j]!=word.charAt(idx)) return false;
-        if(idx==word.length()-1)
-        {
-            return true;
-        }
-
+        if(visited[i][j]) return false;
+        if(board[i][j]!=word.charAt(idx)) return false;
+        if(idx==word.length()-1) return true; // String Matched.
         visited[i][j]=true;
-
-        if(j-1>=0) if(search(idx+1,word,board,i,j-1,visited)) return true;
-        if(j+1<m)  if (search(idx+1,word,board,i,j+1,visited)) return true;
-        if(i-1>=0) if(search(idx+1,word,board,i-1,j,visited)) return true;
-        if(i+1<n)  if(search(idx+1,word,board,i+1,j,visited)) return true;
-        //Backtrack.
+        boolean find=(explore(idx+1,board,word,visited,i-1,j) ||
+                        explore(idx+1,board,word,visited,i+1,j) ||
+                        explore(idx+1,board,word,visited,i,j-1) ||
+                        explore(idx+1,board,word,visited,i,j+1));
+        if(find) return true;
+        //Back track.
         visited[i][j]=false;
 
         return false;
-
-
 
     }
     public boolean exist(char[][] board, String word) 
     {
         int n=board.length;
         int m=board[0].length;
-       for(int i=0;i<n;i++)
-       {
-        for(int j=0;j<m;j++)
+        for(int i=0;i<n;i++)
         {
-            if(search(0,word,board,i,j,new boolean[n][m])) return true;
-
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]!=word.charAt(0)) continue;
+                if(explore(0,board,word,new boolean[n][m],i,j))
+                {
+                    return true;
+                }
+            }
         }
-        }
-        return false;
+       return false;
     }
 }
