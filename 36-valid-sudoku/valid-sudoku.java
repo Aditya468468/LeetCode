3a03983d370cca1b4fda65d8046c9586695d229c
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-16 09:05:52 +0530

    Time: 5 ms (33.39%) | Memory: 47 MB (29.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,39 +1,46 @@
-class Solution {
+class Solution 
+{
     public boolean isValidSudoku(char[][] board) 
     {
-        HashMap<Integer,Set<Character>> rows=new HashMap<>();
-        HashMap<Integer,Set<Character>> cols=new HashMap<>();
-        HashMap<Integer,Set<Character>> grid=new HashMap<>();
+        HashMap<Integer,Set<Integer>> row=new HashMap<>();
+        HashMap<Integer,Set<Integer>> col=new HashMap<>();
+        HashMap<Integer,Set<Integer>> grid=new HashMap<>();
         int n=board.length;
         int m=board[0].length;
-
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-    
-                char c=board[i][j];
-                if(c=='.') continue;
-                if(rows.containsKey(i) && rows.get(i).contains(c)) return false;
-                if(cols.containsKey(j) && cols.get(j).contains(c)) return false;
-                // i need grid num.
+                if(board[i][j]=='.') continue;
+                int num=board[i][j]-'0';
+                if(row.containsKey(i) && row.get(i).contains(num)) return false;
+                if(col.containsKey(j) && col.get(j).contains(num)) return false;
+                int gridNo=(i/3)*3 + j/3;
+                if(grid.containsKey(gridNo) && grid.get(gridNo).contains(num))
+                {
+                    return false;
+                }
+                if(!row.containsKey(i))
+                {
+                    row.put(i,new HashSet<>());    
+                }
+                if(!col.containsKey(j))
+                {
+                    col.put(j,new HashSet<>());    
+                }
+                if(!grid.containsKey(gridNo))
+                {
+                    grid.put(gridNo,new HashSet<>());    
+                }
+                row.get(i).add(num);
+                col.get(j).add(num);
+                grid.get(gridNo).add(num);
                 
-                int gridNo = (i/3)*3 + (j/3);;
-                if(grid.containsKey(gridNo) &&grid.get(gridNo).contains(c)) return false;
-                //Put them
-                if(!rows.containsKey(i)) rows.put(i,new HashSet<>());
-                if(!cols.containsKey(j)) cols.put(j,new HashSet<>());
-                if(!grid.containsKey(gridNo)) grid.put(gridNo,new HashSet<>());
-                rows.get(i).add(c);
-                cols.get(j).add(c);
-                grid.get(gridNo).add(c);
-
-
             }
-
         }
 
-        return true;
         
+        return true;
+
     }
 }
