c8a09b5ade290a0d050942f9a01cabede9ad06e2
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-20 18:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (5.91%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -12,24 +12,24 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) 
     {
         if(head==null || head.next==null) return head;
-        ListNode temp=head.next;;
-        ListNode prev=head;
-        while(temp!=null)
-        {
-            if(temp.val==prev.val)
-            {
-                temp=temp.next;
-            }
-            else
-            {
-                prev.next=temp;
-                prev=temp;
-                temp=temp.next;
-            }
-        }
-        prev.next=null;// Last Connection.
-        return head;
 
+        ListNode prev=head; 
+        // Single element is always unique, prev tracks the prev unique element
+        ListNode curr=head.next; //traversal.
+        while(curr!=null)
+        {
+            ListNode nextNode=curr.next;
+            curr.next=null; //isolate it
+            if(prev.val!=curr.val)
+            {
+                prev.next=curr;
+                prev=curr;
+            }
+             curr=nextNode;
+        }
+        prev.next=null; // Any Last connections.
+
+        return head;
         
     }
 }
