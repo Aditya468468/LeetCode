0aaa7c7545017d4b9432b052b3166ae5b6960a23
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-22 22:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (62.30%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -9,7 +9,18 @@
 class Solution {
     public void deleteNode(ListNode node) 
     {
+        ListNode nextNode=node.next;
         node.val=node.next.val;
         node.next=node.next.next;
+        nextNode.next=null; //Clean Up
     }
+        // A funny Ques, Haha,
+        // See we dont need to delete its memory and all,
+        // If node is give if i change its val to its next node val,
+        // then make it point to next to next node, Then BOOM Done.
+        // 4->5->1->
+
+        //      |->->->|    
+        //   4->1  1-> 9 see how flow changes.
+    
 }
