dfac3090b05f2a797b5b847c30ea4c3dfc9ebeca
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-27 14:03:46 +0530

    Time: 1 ms (99.94%) | Memory: 46.4 MB (53.49%) - LeetSync

---
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution 
+{
+    public ListNode merge(ListNode headA,ListNode headB)
+    {
+        ListNode temp1=headA;
+        ListNode temp2=headB;
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy; 
+        while(temp1!=null && temp2!=null)
+        {
+            if(temp1.val>temp2.val)
+            {
+                curr.next=temp2;
+                curr=temp2;
+                temp2=temp2.next;
+            }
+            else
+            {
+                curr.next=temp1;
+                curr=temp1;
+                temp1=temp1.next;
+            }
+        }
+        if(temp1==null)
+        {
+            curr.next=temp2;
+        }
+        else
+        {
+            curr.next=temp1;
+        }
+
+        return dummy.next;
+
+    }
+    public ListNode middleNode(ListNode head)
+    {
+        if(head==null || head.next==null) return head;
+        ListNode fast=head.next; // We need the First Middle node in even-len,As it will be our left half.
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+
+        return slow;
+
+    }
+    public ListNode mergeSort(ListNode head)
+    {
+      
+        if(head==null || head.next==null) return head;
+        ListNode mid=middleNode(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        return merge(mergeSort(head),mergeSort(right));
+    
+    }
+    public ListNode insertionSortList(ListNode head)
+    {
+        return mergeSort(head);
+    }
+}
+
