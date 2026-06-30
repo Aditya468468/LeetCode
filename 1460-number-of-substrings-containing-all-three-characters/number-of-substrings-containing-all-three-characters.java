aea0497e0bde530321848f91dc42183e3a953eb1
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-06-30 05:55:45 +0530

    Time: 10 ms (95.55%) | Memory: 46.4 MB (45.69%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,33 +1,24 @@
-class Solution 
-{
-    public int atMost(String s, int x)
-    {
-        HashMap<Character,Integer> map=new HashMap<>();
-        int cnt=0;
-        int left=0;
-        for(int right=0;right<s.length();right++)
-        {
-            char ch=s.charAt(right);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            while(map.size()>x)
-            {
-                char c=s.charAt(left);
-                map.put(c,map.get(c)-1);
-                if(map.get(c)==0)
-                {
-                    map.remove(c);
-                }
-                left++;
-            }
-            cnt+=(right-left+1);
-
-        }
-
-        return cnt;
-    }
+class Solution {
     public int numberOfSubstrings(String s) 
     {
-        return atMost(s,3)-atMost(s,2);
+        int lastIdxA=-1;
+        int lastIdxB=-1;
+        int lastIdxC=-1;
+        int cnt=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char x=s.charAt(i);
+            if(x=='a') lastIdxA=i;
+            else if(x=='b') lastIdxB=i;
+            else lastIdxC=i;
+            int min=(Math.min(lastIdxA,Math.min(lastIdxB,lastIdxC)));
+            if(min!=-1)
+            {
+                cnt+=(min+1);
+            }
+        }
+        
+        return cnt;
         
     }
 }
