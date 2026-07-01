752f4ff63ad809165f8034cd7f6b8504fccd4d06
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-01 12:36:17 +0530

    Time: 11 ms (37.15%) | Memory: 59.2 MB (85.75%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,67 +10,61 @@
  */
 class Solution 
 {
-    public ListNode merge(ListNode headA,ListNode headB)
+    public ListNode merge(ListNode list1,ListNode list2)
     {
-        ListNode temp1=headA;
-        ListNode temp2=headB;
         ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy; 
+        ListNode curr=dummy;
+        ListNode temp1=list1;
+        ListNode temp2=list2;
         while(temp1!=null && temp2!=null)
         {
-            if(temp1.val>temp2.val)
+            if(temp1.val<temp2.val)
             {
-                curr.next=temp2;
-                curr=temp2;
-                temp2=temp2.next;
+                ListNode nextNode=temp1.next;
+                curr.next=temp1;
+                curr=curr.next;
+                temp1.next=null;
+                temp1=nextNode;
             }
             else
             {
-                curr.next=temp1;
-                curr=temp1;
-                temp1=temp1.next;
+                ListNode nextNode=temp2.next;
+                curr.next=temp2;
+                curr=curr.next;
+                temp2.next=null;
+                temp2=nextNode;
+
             }
         }
-        if(temp1==null)
-        {
-            curr.next=temp2;
-        }
-        else
-        {
-            curr.next=temp1;
-        }
+        if(temp1!=null) curr.next=temp1;
+        else curr.next=temp2;
 
         return dummy.next;
 
     }
-    public ListNode middleNode(ListNode head)
+    public ListNode middle(ListNode head)
     {
         if(head==null || head.next==null) return head;
-        ListNode fast=head.next; // We need the First Middle node in even-len,As it will be our left half.
+        ListNode fast=head.next;
         ListNode slow=head;
         while(fast!=null && fast.next!=null)
         {
             fast=fast.next.next;
             slow=slow.next;
         }
-
         return slow;
-
     }
     public ListNode mergeSort(ListNode head)
     {
-      
         if(head==null || head.next==null) return head;
-        ListNode mid=middleNode(head);
-        ListNode right = mid.next;
-        mid.next = null;
-
+        ListNode mid=middle(head);
+        ListNode right=mid.next;
+        mid.next=null;
         return merge(mergeSort(head),mergeSort(right));
-    
     }
     public ListNode sortList(ListNode head) 
     {
         return mergeSort(head);
+        
     }
 }
-
