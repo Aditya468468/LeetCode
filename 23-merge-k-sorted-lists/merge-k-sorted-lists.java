aaae112bd53cb0577dd098c99bd1c6c434c83c69
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-01 07:40:57 +0530

    Time: 4 ms (82.29%) | Memory: 46.9 MB (45.51%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,54 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+class Pair
+{
+    int val;
+    ListNode node;
+    public Pair(int val,ListNode node)
+    {
+        this.val=val;
+        this.node=node;
+    }
+}
 class Solution 
 {
-    public ListNode merge(ListNode headA,ListNode headB)
-    {
-        ListNode temp1=headA;
-        ListNode temp2=headB;
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy; 
-        while(temp1!=null && temp2!=null)
-        {
-            if(temp1.val>temp2.val)
-            {
-                curr.next=temp2;
-                curr=temp2;
-                temp2=temp2.next;
-            }
-            else
-            {
-                curr.next=temp1;
-                curr=temp1;
-                temp1=temp1.next;
-            }
-        }
-        if(temp1==null)
-        {
-            curr.next=temp2;
-        }
-        else
-        {
-            curr.next=temp1;
-        }
-
-        return dummy.next;
-
-    }
     public ListNode mergeKLists(ListNode[] lists) 
     {
         if(lists.length==0) return null;
         if(lists.length==1) return lists[0];
-        ListNode newHead=merge(lists[0],lists[1]);
-        ListNode currHead=newHead;
-        for(int i=2;i<lists.length;i++)
+        PriorityQueue<Pair> q=new PriorityQueue<>((a,b)->a.val-b.val);
+        // As its a custom class PQ doesnt know how to sort it, So passed a custom comp
+        for(ListNode x:lists)
         {
-            newHead=merge(currHead,lists[i]);
-            currHead=newHead;
+            if(x!=null) q.offer(new Pair(x.val,x));
+        }
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+        while(!q.isEmpty())
+        {
+            Pair p =q.poll();
+            curr.next=p.node;
+            curr=curr.next;
+            if(p.node.next!=null)
+            {
+                q.offer(new Pair(p.node.next.val,p.node.next));
+            }
         }
 
-        return currHead;
+        return dummy.next;
+
         
     }
 }
