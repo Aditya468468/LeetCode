44df26ce054534888fb65ef40002b753d64646eb
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-03 10:12:59 +0530

    Time: 1 ms (97.65%) | Memory: 42.5 MB (78.06%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,23 +1,21 @@
-class Solution {
-    public int mySqrt(int n) 
+class Solution 
+{
+
+    public int mySqrt(int x) 
     {
-        long low=1;
-        long high=n;
+        long low=0;
+        long high=x;
         while(low<=high)
         {
             long mid=low+(high-low)/2;
-            if(mid*mid<=n)
-            {
-                low=mid+1;
-            }
-            else 
-            {
-                high=mid-1;
-            }
+            long val=mid*mid;
+            if(val==x) return (int)mid;
+            else if(val<x) low=mid+1;
+            else high=mid-1;
         }
 
         return (int)high;
-
-      
+        
+        
     }
 }
