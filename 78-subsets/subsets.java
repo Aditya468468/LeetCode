9567163d4dd81f6b6868db99b8e1dd5d13346621
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-11 06:11:17 +0530

    Time: 1 ms (87.27%) | Memory: 44.4 MB (20.64%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,26 +1,23 @@
+
 class Solution 
 {
-    private void generate(int idx,int[]nums,List<Integer> list,List<List<Integer>> ans)
-    {
-        //Whenever we have 2 options a Idx-> We use Pick/Not Pick or
-        //Pick A/Pick B etc 
-        if(idx==nums.length)
-        {
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        //Take That Idx,
-        list.add(nums[idx]);
-        generate(idx+1,nums,list,ans);
-        //Backtrack/Undo it-> As List have a same ref-> Need to undo to try others.
-        list.remove(list.size()-1);
-        //Not take.
-        generate(idx+1,nums,list,ans); //Just move to next idx.
-    }
+
     public List<List<Integer>> subsets(int[] nums) 
     {
-        List<List<Integer>> ans=new ArrayList<>();
-        generate(0,nums,new ArrayList<>(),ans);
+        int n=nums.length;
+        int elem=1<<n;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0;i<elem;i++)
+        {
+        
+            List<Integer> list=new ArrayList();
+            for(int j=0;j<n;j++)
+            {
+                if((i&(1<<j))!=0) list.add(nums[j]);
+            }
+            ans.add(list);
+        }
 
         return ans;
 
