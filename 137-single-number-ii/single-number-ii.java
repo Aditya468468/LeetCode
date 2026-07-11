81f956ef5e56c2420fd7843f33a6c590c980d0e8
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-11 10:40:50 +0530

    Time: 4 ms (64.05%) | Memory: 45.4 MB (40.33%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,30 +1,27 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        
-        Arrays.sort(nums);
-        int n=nums.length;
-        if(n==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low=1;
-        int high=nums.length-2;
-        while(low<=high)
+    public int singleNumber(int[] nums) 
+    {
+        int ans=0;
+        for(int i=0;i<32;i++)
         {
-            int mid=low+(high-low)/2;
-            mid=mid-mid%3;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+            int countSet=0;
+            for(int j=0;j<nums.length;j++)
             {
-                return nums[mid];
+                if((nums[j]&(1<<i))!=0)
+                {
+                    countSet++;
+                }
+
             }
-            if(nums[mid]==nums[mid+1] && nums[mid+1]==nums[mid+2])
+            if(countSet%3!=0)
             {
-                low=mid+3;
+                ans=(1<<i) | ans;
             }
-            else
-            {
-                high=mid;
-            }
+            
         }
-        return -1;
+
+        return ans;
+
+
     }
 }
