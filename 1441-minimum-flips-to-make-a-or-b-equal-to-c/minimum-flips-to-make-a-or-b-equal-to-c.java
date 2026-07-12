2c73a67840cd1012dfd856329cd2cff5daf984a1
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-12 11:44:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (41.99%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,41 +1,30 @@
 class Solution {
     public int minFlips(int a, int b, int c) 
     {
-        int flips=0;
-
+        int minFlips=0;
         while((a|b)!=c)
         {
-            int bitC=(c&(1));
             int bitA=(a&1);
             int bitB=(b&1);
-            if(bitC==1)
+            int bitC=(c&1); //These Tells, does that bit, it 0 or 1 
+
+            //Lets process
+            if(bitC==1) //Turn Any One Bit 1
             {
-                if(bitA!=1 && bitB!=1) // We need 1, soo turing any bit On is fine.
-                {
-                    flips++;
-                }
+                if(bitA!=1 && bitB!=1) minFlips+=1;
             }
-            else if(bitC==0)
+            else if(bitC==0) // Turn both Bit 0.
             {
-                if(bitA==1 && bitB==1) // 1 | 1 =1 soo need to turn both off
-                {
-                    flips=flips+2;
-                }
-                else if(bitA==1 && bitB==0) // 1|0-> turn one bit off
-                {
-                    flips++;
-                }
-                else if(bitA==0 && bitB==1) // same 
-                {
-                    flips++;
-                }
+                if(bitA==1 && bitB==1) minFlips+=2; // need to turn off 2
+                else if(bitA==1 && bitB==0) minFlips+=1; // Need to turn 1
+                else if(bitA==0 && bitB==1) minFlips+=1; // Need to turn 1
             }
             a=a>>1;
             b=b>>1;
-            c=c>>1;
+            c=c>>1;  // Drop bits.
         }
 
-        return flips;
+        return minFlips;
         
     }
 }
