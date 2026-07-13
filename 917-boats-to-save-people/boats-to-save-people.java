4cfa88dbb21c4623b1ef6e0406b18f93618d9af5
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-13 16:33:28 +0530

    Time: 19 ms (98.47%) | Memory: 56.5 MB (56.55%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) 
     {
-        Arrays.sort(people);
+        Arrays.sort(people); // In order to organise weights to choose effi
         int cnt=0;
         int n=people.length;
         int i=0;
         int j=n-1;
         while(i<=j)
         {
-            if(people[i]+people[j]<=limit)
+            if(people[i]+people[j]<=limit) // The best Pair is Heavy(As we need to fit he first+ Light (as A filler for space))
             {
                 i++;
-                j--;
+                j--; //Paired
             }
             else
             {
                 //if(people[j]<=limit) Not needed due to Constraints;
-                j--;
+                j--; // If we cant make pair with ith , We cant make pair with (i+1)th as they are more heavier.. (Sorted) ,So He needs to Go Alone.
             }
              cnt++;
         }
