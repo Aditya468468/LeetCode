37651f1b50a3e9f96aa81303cabefa7dacf6cf22
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-13 11:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (36.36%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,50 +1,49 @@
-class Solution 
-{
-    public void generate(int curr,List<Integer> list,StringBuilder str,int prev,int low, int high)
+class Solution {
+
+    public int countDigits(int num)
     {
-       
-        if(str.length()>=2)
+        int len=0;
+        while(num!=0)
         {
-            int num = Integer.parseInt(str.toString());
-             if(num>=low && num<=high)
-             {
-                list.add(num);
-             }
-             if(num>high)     
-                return;
+            len++;
+            num/=10;
         }
+        return len;
+    }
+    public void generateSeq(String ref, int low,int high,int k,List<Integer> list)
+    {
 
-        if(curr>9 || (curr-prev!=1 && prev!=-1)) return;
-
-        if(prev==-1)
+        int left=0;
+        for(int right=0;right<ref.length();right++)
         {
-            //Start
-            str.append(curr);
-            generate(curr+1,list,str,curr,low,high);
-            str.deleteCharAt(str.length()-1);
-
-            // Dont start
-            generate(curr+1,list,str,-1,low,high);
-        }
-        else
-        {
-            //pick
-            str.append(curr);
-            generate(curr+1,list,str,curr,low,high);
-            str.deleteCharAt(str.length()-1);
-
-        }
-      
+            while(right-left+1>k)
+            {
+                left++;
+            }
+            int len=right-left+1;
+            if(len==k)
+            {
+                int num=Integer.parseInt(ref.substring(left,right+1));
+                if(num>=low && num<=high) list.add(num);
         
+            }
+        }
+
     }
     public List<Integer> sequentialDigits(int low, int high) 
     {
-        List<Integer> list = new ArrayList<>();
-        generate(1,list,new StringBuilder(),-1,low,high);
-
+        String ref="123456789";
+        List<Integer> list=new ArrayList<>();
+        int min=countDigits(low);
+        int max=countDigits(high);
+        int left=0;
+        for(int i=min;i<=max;i++)
+        {
+            generateSeq(ref,low,high,i,list);
+        }
         Collections.sort(list);
         return list;
-    
+
         
     }
 }
