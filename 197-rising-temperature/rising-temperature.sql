2c6e3a314666d71d3293c878a9a9e74aafb504e2
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-15 12:51:24 +0530

    Time: 336 ms (7.39%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,5 +2,5 @@ SELECT
     currDate.id
 FROM Weather AS currDate
 INNER JOIN Weather AS prevDate
-ON DATEDIFF(currDate.recordDate,prevDate.recordDate)=1 
+ON currDate.recordDate - prevDate.recordDate = 1
 AND currDate.temperature>prevDate.temperature;
