6ee04811da89d30c8b5565d4954a4d3dc55a3cd8
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-15 16:22:46 +0530

    Time: 797 ms (15.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,12 @@
+SELECT
+    manager.employee_id,
+    manager.name,
+    COUNT(*) reports_count,
+    ROUND(AVG(emp.age)) average_age
+FROM Employees AS manager
+JOIN Employees AS emp
+ON manager.employee_id=emp.reports_to
+GROUP BY manager.employee_id,manager.name
+ORDER BY manager.employee_id;
+
+
