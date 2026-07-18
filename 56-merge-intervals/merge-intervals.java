5931d5731449397f79456b2fa6264d3ecc7b2f2e
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-18 06:26:07 +0530

    Time: 9 ms (36.58%) | Memory: 49.2 MB (27.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,41 +1,37 @@
 class Solution {
     public int[][] merge(int[][] intervals) 
     {
-        int n=intervals.length;
-        if(n==1) return intervals;
-        ArrayList<int[]> list=new ArrayList<>();
         Arrays.sort(intervals,(a,b)->
         {
             if(a[0]==b[0]) return a[1]-b[1];
             return a[0]-b[0];
         });
-        //Non-overlap cond-> a.endTime<b.startTime
-        //OverLap--> ~(Non-overlap)--> a.endTime>=b.startTime
-        list.add(intervals[0]); //Soo i can have a prev interval.
-        
-        for(int i=1;i<n;i++)
+
+        List<int[]> list=new ArrayList<>();
+        list.add(intervals[0]);
+
+        for(int i=1;i<intervals.length;i++)
         {
-            int[] prevInterval=list.get(list.size()-1);
+            int []prevInterval=list.get(list.size()-1);
             if(prevInterval[1]>=intervals[i][0])
             {
-                prevInterval[1]=Math.max(intervals[i][1],prevInterval[1]); // Update the endPoint
-
+                prevInterval[1]=Math.max(intervals[i][1],prevInterval[1]);
             }
-            else // Non overlap, Add it 
+            else
             {
                 list.add(intervals[i]);
             }
-
         }
 
-        int [][]result=new int[list.size()][2];
-        for(int i=0;i<list.size();i++)
+        int[][] ans = new int[list.size()][2];
+        int idx=0;
+        for(int[] x:list)
         {
-            result[i]=list.get(i);
-        }
+            ans[idx++]=x;
+        } 
+
+        return ans;
         
-        return result;
-
-
+        
     }
 }
