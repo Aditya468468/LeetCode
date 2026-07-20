eb9fe2259ba3cf9f1e7cea6a35e52437df08bba6
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-20 09:50:54 +0530

    Time: 7 ms (43.22%) | Memory: 47 MB (79.54%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -5,46 +5,28 @@ class Solution
     {
         int n=grid.length;
         int m=grid[0].length;
-        int total=n*m;
-        k=k%total;
-
-        while(k!=0)
-        {
-            for(int j=m-1;j>0;j--)
-            {
-                for(int i=0;i<n;i++)
-                {
-                    //swap
-                    int temp=grid[i][j-1];
-                    grid[i][j-1]=grid[i][j];
-                    grid[i][j]=temp;
-                }
-
-            }
-            //RIGHT Shift 1st cols
-            int last=grid[n-1][0];
-            for (int i=n-1;i>0;i--) 
-            {
-                grid[i][0]=grid[i - 1][0];
-            }
-            grid[0][0]=last;
-            k--;
-        }
-    
-
+        int L=n*m;
+        k=k%L;
         List<List<Integer>> list=new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             List<Integer> temp=new ArrayList<>();
             for(int j=0;j<m;j++)
             {
-                temp.add(grid[i][j]);
-
+                temp.add(0);
             }
             list.add(temp);
         }
 
+        for(int i=0;i<n*m;i++)
+        {
+            int pivot=grid[i/m][i%m];
+            int newIdx=(i+k)%L;
+            List<Integer> temp=list.get(newIdx/m);
+            temp.set(newIdx%m,pivot);
+        }
 
+      
         return list;
 
         
