015b3167d2f16f180c1966b53ee4c4bc60c31386
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-20 13:57:29 +0530

    Time: 934 ms (5.10%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,8 +1,15 @@
-SELECT
-    customer_id 
-FROM Customer
-GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key)=
+SELECT c.customer_id
+FROM Customer c
+GROUP BY c.customer_id
+HAVING NOT EXISTS
 (
-    SELECT COUNT(*) FROM Product
+    SELECT 1
+    FROM Product p
+    WHERE NOT EXISTS
+    (
+        SELECT 1
+        FROM Customer c2
+        WHERE c2.customer_id = c.customer_id
+          AND c2.product_key = p.product_key
+    )
 );
