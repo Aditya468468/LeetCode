dd6441d86ff3d437571f80433edb724dd5810696
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-22 14:42:36 +0530

    Time: 868 ms (20.60%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -4,6 +4,7 @@ SELECT
 FROM Products p
 JOIN Orders o
 ON p.product_id=o.product_id
-WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='2'
+-- WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='2'
+WHERE o.order_date LIKE '2020-02-%'
 GROUP BY p.product_id
 HAVING SUM(o.unit)>=100
