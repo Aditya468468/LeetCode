17ae9552f436b3f34356fa7be497fd30704de58c
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-22 16:29:06 +0530

    Time: 675 ms (40.97%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,22 +1,16 @@
-
+WITH first_order AS (
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_date
+    FROM Delivery
+    GROUP BY customer_id
+)
 SELECT
     ROUND(
-        (SUM(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END)/COUNT(*))*100,2) 
-        AS immediate_percentage
-FROM(
-    SELECT
-    d1.customer_id,
-    d1.delivery_id,
-    d1.order_date,
-    d1.customer_pref_delivery_date
-    FROM Delivery d1
-    WHERE d1.order_date= (
-        SELECT
-        MIN(order_date)
-        FROM Delivery d2
-        WHERE d1.customer_id=d2.customer_id
-    )
- ) AS first_order
-
-    
-
+        AVG(d.order_date = d.customer_pref_delivery_date) * 100,
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN first_order f
+ON d.customer_id = f.customer_id
+AND d.order_date = f.first_date;
