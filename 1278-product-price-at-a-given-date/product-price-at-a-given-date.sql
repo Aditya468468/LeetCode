d8a81867157919afc5fa4ee873efe053d2991628
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-22 23:17:38 +0530

    Time: 694 ms (18.76%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,4 +1,4 @@
-WITH updated AS
+WITH updated AS  -- Find the Latest Dates which updated before 16th as after 16th its irrelevant as they will stay 10 always
 (
     SELECT 
     product_id,
@@ -7,26 +7,22 @@ WITH updated AS
     WHERE change_date <= '2019-08-16'
     GROUP BY product_id
 )
-SELECT 
+SELECT  --  Now check are they got updated before 16th if yes give there new price
     u.product_id,
     p.new_price AS price
 FROM Products as p
 JOIN updated u
 ON p.product_id=u.product_id AND u.max_change_date=p.change_date
 
-
 UNION
 
-SELECT 
+SELECT  --  If Not the Just give there price as 10
     p.product_id,
     10 AS price 
 FROM Products as p
 LEFT JOIN updated u
 ON p.product_id=u.product_id 
 WHERE u.product_id IS NULL;
-
-
-
 
 
    
