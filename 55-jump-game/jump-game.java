d4551268d7c0120aaedbdf5253ac02d3fee34d60
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-27 22:21:45 +0530

    Time: 2 ms (88.82%) | Memory: 47.8 MB (81.57%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,9 +3,11 @@ class Solution {
     {
         int maxReach=0;
         int n=nums.length;
+        if(n==1) return true; //have to handle it 
         for(int i=0;i<n;i++)
         {
-            if(i>maxReach) return false;//Can i even reach that idx?? If not return false
+            // if(i>maxReach) return false;//Can i even reach that idx?? If not return false
+            if(nums[i]==0 && maxReach<=i) return false; // All terminating at Zero
             int reach=i+nums[i];
             maxReach=Math.max(maxReach,reach); //Update
             if(maxReach>=n-1) return true; // We crossed the line , Vamos 
