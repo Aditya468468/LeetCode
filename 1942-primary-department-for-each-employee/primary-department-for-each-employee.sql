2a50c641daea24bf8a15b78ac914efebc508dbba
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-28 00:03:04 +0530

    Time: 604 ms (35.48%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,22 +1,22 @@
 -- Lets Handle Single Dept
-SELECT
-    employee_id,
-    department_id
-FROM Employee
-GROUP BY employee_id 
-HAVING COUNT(department_id)=1
+WITH temp AS (
+    SELECT 
+        employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+)
+SELECT 
+    e.employee_id,
+    e.department_id
+FROM Employee e
+INNER JOIN temp ON temp.employee_id = e.employee_id
 
 UNION ALL
--- Lets Handle Multiple Dept 
-SELECT
+
+-- Lets Handle Multiple Dept
+SELECT 
     employee_id,
     department_id
 FROM Employee
-WHERE primary_flag='Y'
-
-
-
-
-
-
-
+WHERE primary_flag = 'Y';
