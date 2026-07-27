36abe6c5c348aab79e2027ae0824fb73e5fe0a95
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-27 23:58:41 +0530

    Time: 602 ms (36.45%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -7,13 +7,12 @@ GROUP BY employee_id
 HAVING COUNT(department_id)=1
 
 UNION ALL
-
+-- Lets Handle Multiple Dept 
 SELECT
     employee_id,
     department_id
 FROM Employee
 WHERE primary_flag='Y'
-GROUP BY employee_id 
 
 
 
