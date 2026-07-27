1d48087353dc143a4df7de63f93112ecfca587e8
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-27 23:53:06 +0530

    Time: 612 ms (32.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,23 @@
+-- Lets Handle Single Dept
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+GROUP BY employee_id 
+HAVING COUNT(department_id)=1
+
+UNION ALL
+
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag='Y'
+GROUP BY employee_id 
+
+
+
+
+
+
+
