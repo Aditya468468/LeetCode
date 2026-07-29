75fd11f9b2e2336e0531de20eed39d1618e51785
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-29 09:53:24 +0530

    Time: 1 ms (62.11%) | Memory: 43.7 MB (67.09%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) 
+    {
+        int n=code.length;
+        int[]ans=new int[n];
+        if(k==0)
+        {
+            return ans;
+        }
+        int sum=0;
+        int left=0;
+        boolean parity=k>0;
+        if(k<0)
+        {
+            k=k*(-1);
+        }
+        for(int right=0;right<2*n;right++)
+        {
+            sum+=code[right%n];
+            
+            while(right-left+1>k)
+            {
+                sum-=code[left%n];
+                left++;
+            }
+            if(right-left+1==k)
+            {
+                int idx=-1;
+                if(!parity)
+                {
+                  idx=(right+1)%n;
+                }
+                else
+                {
+                    idx=(left-1+n)%n;
+                }
+                ans[idx]=sum;
+            }
+        }
+
+
+        return ans;
+
+        
+    }
+}
