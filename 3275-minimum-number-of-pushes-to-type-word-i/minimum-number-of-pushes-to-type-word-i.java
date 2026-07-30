eb2a5dae2f49051f79f8fd51c917b3d216eb54e1
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-07-30 07:22:55 +0530

    Time: 2 ms (21.05%) | Memory: 43.5 MB (16.51%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,31 +1,32 @@
 class Solution {
     public int minimumPushes(String word) 
     {
-        int []freq=new int[26];
-        for(int i=0;i<word.length();i++)
+        int n=word.length();
+        int[]freq=new int[26];
+        for(int i=0;i<n;i++)
         {
-            char c=word.charAt(i);
-            freq[c-'a']++;
+            char ch=word.charAt(i);
+            freq[ch-'a']++;
         }
-        Arrays.sort(freq);
-        long cnt=0;
-        long score=1;
-        long min=0;
-        for(int i=freq.length-1;i>=0;i--)
+
+        Arrays.sort(freq); //We Sort it by freq
+        int keyUsed=0; // Used digits,As we can only use 8 digits-->digits/8
+        int min=0;
+        int times=1; // initally we can press all key 1 times
+        for(int i=freq.length-1;i>=0;i--)//by desc order so higher freq can be mapped to lower ones
         {
-            if(freq[i]!=0)
+            int freqOf=freq[i];
+            if(freq[i]==0) continue;
+            if(keyUsed==8)//used all 8 keys
             {
-                min+=(1L)*(freq[i]*score);
-                cnt++;
+                times++; //Times inc
+                keyUsed=0; //Reset 
+
             }
-            if(cnt==8)
-            { 
-                score++; //Will score more now
-                cnt=0;
-            }
+            min+=(1L)*(times*freqOf);
+            keyUsed++;
         }
 
-
-        return (int)min;
+        return min;
     }
 }
