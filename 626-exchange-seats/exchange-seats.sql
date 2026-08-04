bb1fe298c0039d761bff981906edaa70033e3746
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-08-04 21:12:42 +0530

    Time: 426 ms (16.17%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,12 +1,18 @@
-SELECT
+WITH consecutive_seat AS 
+(
+    SELECT
     s1.id,
     CASE 
-    WHEN s1.id%2!=0 THEN COALESCE((SELECT student FROM Seat s2 WHERE s2.id=s1.id+1),s1.student)
-    WHEN s1.id%2=0 THEN (SELECT student FROM Seat s2 WHERE
-    s2.id=s1.id-1)
-    END student
-FROM Seat s1
+        WHEN s1.id%2=0 THEN (SELECT s2.id FROM Seat s2 WHERE s2.id=s1.id-1) 
+        ELSE (SELECT s2.id FROM Seat s2 WHERE s2.id=s1.id+1) 
+    END AS cons_id
+    FROM Seat s1 
+)
 
-
-
-
+SELECT 
+    COALESCE(c.id,s.id) AS id,
+    s.student
+FROM Seat s 
+LEFT JOIN consecutive_seat c
+ON s.id=c.cons_id
+ORDER BY id;
