4ad9567b4a19bc4180ee445e246b6793acb5a7db
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-08-15 17:17:20 +0530

    Time: 88 ms (32.08%) | Memory: 89.1 MB (97.42%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,45 +1,73 @@
-class Solution {
-    private class Pair
-    {
-        int pos;
-        double time;
-        
-        public Pair(int pos,double time)
-        {
-            this.pos=pos;
-            this.time=time;
-        }
-    }
-    public int carFleet(int target, int[] position, int[] speed) 
-    {
-        int n=position.length;
-        Pair[] nums=new Pair[n];
-        for(int i=0;i<n;i++)
-        {
-            int pos=position[i];
-            double time=(double)(target-pos)/speed[i];
-            nums[i]=new Pair(pos,time);
-        }
-        Arrays.sort(nums,(a,b)->
-        {
-            return b.pos-a.pos;
-        });
-        // Lets See fleets.
-        double prevTime=0;
-        int fleet=0;
-        for(int i=0;i<n;i++)
-        {
-            double current=nums[i].time;
-            if(current>prevTime)
-            {
-                fleet++;
-                prevTime=current;
+class Car
+{
+    int position;
+    int speed;
 
-            }
-        }
-
-        return fleet;
-        
+    public Car(int position,int speed)
+    {
+        this.position=position;
+        this.speed=speed;
     }
 }
+class Solution 
+{
 
+    public int carFleet(int target, int[] position, int[] speed)
+    {
+        int n=speed.length;
+        List<Car> cars=new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            cars.add(new Car(position[i],speed[i]));
+        }
+        
+        cars.sort((a,b)->
+        {
+            return a.position-b.position;
+        });
+        
+        Stack<Car> st = new Stack<>();
+
+        for(int i=n-1;i>=0;i--) // It will help us more, i can decide can ith catch top's fleet, rather then can top fleet can catch ith
+        { 
+            if(st.isEmpty())
+            {
+                st.push(cars.get(i));
+            }
+            else
+            {
+                int s=cars.get(i).speed;
+                int p= cars.get(i).position;
+                if(s>st.peek().speed) //it can catch.
+                {
+                    //but will it catch within target
+                    double relativeDistance=st.peek().position-p;
+                    double relativeSpeed=s-st.peek().speed;
+                    double time=relativeDistance/relativeSpeed;
+                    double newPosition=p+(time*s);
+                    if(newPosition<=target) // Yes it will catch it
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        st.push(cars.get(i)); // It cant catch will be a part of new fleet.
+                    }
+                    
+                }
+                else
+                {
+                    st.push(cars.get(i));
+                    // if speed of ith is less or equal, it cant catch,
+                    // Will be part of new fleet.
+                }
+            }
+
+        }
+
+        return st.size();
+
+
+    }
+}
