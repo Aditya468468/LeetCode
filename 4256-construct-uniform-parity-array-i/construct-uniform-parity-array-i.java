33c99f2678860e6a45f1c20441c98d133491953f
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-09-02 06:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (73.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,39 +1,8 @@
 class Solution {
     public boolean uniformArray(int[] nums1) 
     {
-        // Odd-even/Even-Odd-> 3-2=1 -> Odd 
-        // Even-Even=Even-> 8-4=4
-        // Odd-Odd=7-3=3--> Even
-
-        //All Odds
-
-        int oddCount=0;
-        int evenCount=0;
-
-        for(int i=0;i<nums1.length;i++)
-        {
-            if(nums1[i]%2==0)
-            {
-                evenCount++;
-            }
-            else
-            {
-                oddCount++;
-            }
-        }
-
-        if(evenCount==0 || oddCount==0 || evenCount>0 || oddCount>0)
-        {
-            return true;
-        }
-
-
-        return false;
-
-
-
-
-
+       
+        return true;
         
     }
 }
