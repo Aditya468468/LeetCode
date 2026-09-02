b43e6258dfcc73338ab7d1c3050c9ac21562a406
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-09-02 06:20:17 +0530

    Time: 1 ms (31.89%) | Memory: 45.3 MB (16.73%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,50 +1,39 @@
 class Solution {
     public boolean uniformArray(int[] nums1) 
     {
-        int countOdd=0;
-        int countEven=0;
-        for(int i=0;i<nums1.length;i++)
-            {
-                if(nums1[i]%2==0)
-                {
-                    countEven++;
-                }
-                else
-                {
-                    countOdd++;
-                }
-            }
-        // All odd.
-        boolean isOdd=true;
-        for(int i=0;i<nums1.length;i++)
-            {
-              if(nums1[i]%2==0)
-              {
-                  if(countOdd==0)
-                  {
-                     isOdd=false;
-                      break;
-                  }
-              }
-            }
-        //Even 
-        boolean isEven=true;
-        for(int i=0;i<nums1.length;i++)
-            {
-              if(nums1[i]%2!=0)
-              {
-                  if(countEven==0)
-                  {
-                      isEven=false;
-                      break;
-                  }
-              }
-            }
+        // Odd-even/Even-Odd-> 3-2=1 -> Odd 
+        // Even-Even=Even-> 8-4=4
+        // Odd-Odd=7-3=3--> Even
 
-        return (isOdd || isEven);
-        
-        
-        
+        //All Odds
+
+        int oddCount=0;
+        int evenCount=0;
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            if(nums1[i]%2==0)
+            {
+                evenCount++;
+            }
+            else
+            {
+                oddCount++;
+            }
+        }
+
+        if(evenCount==0 || oddCount==0 || evenCount>0 || oddCount>0)
+        {
+            return true;
+        }
+
+
+        return false;
+
+
+
+
+
         
     }
 }
