669067fb0dcbb1a9e23f15857885a4e3d86da7c5
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-09-11 14:13:37 +0530

    Time: 239 ms (70.12%) | Memory: 110.5 MB (24.64%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,59 +1,44 @@
-
 class Solution {
-    public int sumSubarrayMins(int[] arr) 
+    public int sumSubarrayMins(int[] nums) 
     {
-
-        long mod = 1_000_000_007;
-        int n=arr.length;
-        int []nseR=new int[n];
-        int []nseL=new int[n];
+        int n=nums.length;
+        int[]nsE=new int[n];
+        int[]pseE=new int[n];
         Stack<Integer> st=new Stack<>();
-        //nseR
         for(int i=0;i<n;i++)
         {
-            if(!st.isEmpty())
+            while(!st.isEmpty() && nums[st.peek()]>nums[i])
             {
-                while(!st.isEmpty() && arr[st.peek()]>arr[i])
-                {
-                    nseR[st.pop()]=i;
-                }
+                nsE[st.pop()]=i;
             }
             st.push(i);
         }
         while(!st.isEmpty())
         {
-            nseR[st.pop()]=n;
+            nsE[st.pop()]=n;
         }
-
-        //nseEL-> next smaller or equal Element towards left
-        // [1,1]-> [1],[1,1],[1],[1,1]-> Here [1,1] Overlaps, we taking same sub-array twice-> Soo we can either take the equality from Right or Left  
+        //prevSmallerOrEqual
         for(int i=n-1;i>=0;i--)
         {
-            if(!st.isEmpty())
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i])
             {
-                while(!st.isEmpty() && arr[st.peek()]>=arr[i]) // Taking equal elemets also 
-                {
-                    nseL[st.pop()]=i;
-                }
+                pseE[st.pop()]=i;
             }
             st.push(i);
         }
         while(!st.isEmpty())
         {
-            nseL[st.pop()]=-1;
+            pseE[st.pop()]=-1;
         }
-        
         long sum=0;
-
+        long MOD=1_000_000_007;
         for(int i=0;i<n;i++)
         {
-            sum+=(1L)*(arr[i])*((i-nseL[i])*(nseR[i]-i));
+            long contri=((1L)*(nums[i])*((nsE[i]-i)*(i-pseE[i])))%MOD;
+            sum+=(contri)%MOD;
         }
 
-        return (int)(sum%mod);
-
-
-
-          
+        return (int)(sum%MOD);
+   
     }
 }
