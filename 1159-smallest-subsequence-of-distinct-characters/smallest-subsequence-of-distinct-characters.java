57c6085a1d75d4911296967067625767e432f456
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-09-12 18:58:23 +0530

    Time: 5 ms (6.71%) | Memory: 43.1 MB (10.86%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String smallestSubsequence(String s) 
+    {
+        int n=s.length();
+        Set<Character> set=new HashSet<>();
+        // Will Track elements in Set currently
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            char x=s.charAt(i);
+            map.put(x,i);
+        }
+
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0;i<n;i++)
+        {
+            if(set.contains(s.charAt(i)))
+            {
+                continue;
+            }
+            while(!st.isEmpty() && s.charAt(st.peek())>s.charAt(i) && map.get(s.charAt(st.peek()))>i)
+            {
+                set.remove(s.charAt(st.pop()));
+
+            }
+            st.push(i);
+            set.add(s.charAt(i));
+        }
+
+        StringBuilder str=new StringBuilder();
+
+        while(!st.isEmpty())
+        {
+            str.append(s.charAt(st.pop()));
+        }
+
+        return str.reverse().toString();
+    
+        
+    }
+}
