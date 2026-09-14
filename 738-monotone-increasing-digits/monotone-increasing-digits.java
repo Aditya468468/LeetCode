a139531cb7e4f4e90e69450418f14d6d224902ab
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2026-09-14 15:35:57 +0530

    Time: 1 ms (90.75%) | Memory: 42.4 MB (30.43%) - LeetSync

--- 738-monotone-increasing-digits/monotone-increasing-digits.java
+++ 738-monotone-increasing-digits/monotone-increasing-digits.java
@@ -5,19 +5,20 @@ class Solution {
         char []nums=Integer.toString(n).toCharArray();
         //Converts, The n to char[]
         int size=nums.length;
+        int marker=size;
         
         for(int i=nums.length-2;i>=0;i--)
         {
             if(nums[i]>nums[i+1])
             {
                 nums[i]--;
-                int j=i+1;
-                while(j<size && nums[j]!='9')
-                {
-                    nums[j]='9';
-                    j++;
-                }
+                marker=i+1;
+               
             }
+        }
+        //Optimization
+        for (int i = marker; i < nums.length; i++) {
+            nums[i]='9';
         }
         int i=0;
         while(i<size && nums[i]=='0')
